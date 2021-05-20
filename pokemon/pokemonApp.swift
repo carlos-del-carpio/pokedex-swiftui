06bcf8ae202f34b790e73970dae065d0a6546878
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-05-19 21:32:52 -0400

    Initial Commit

---
+++ pokemon/pokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApp.swift
+//  pokemon
+//
+//  Created by Carlos Del Carpio on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
